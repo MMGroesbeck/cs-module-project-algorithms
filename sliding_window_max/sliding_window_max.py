931894f7476506e2acbd7b3c2613a892c32998d2
M Groesbeck <m.groesbeck@gmail.com>
2020-07-22 12:47:28 -0700

    First pass for sliding window passed.

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,11 +2,17 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+# First pass: O(n*k); runs O(n) for small k, but can be improved.
 def sliding_window_max(nums, k):
     # Your code here
-
-    pass
-
+    out_arr = []
+    for i in range(len(nums) - k + 1):
+        out_arr.append(nums[i])
+        for j in range(1,k):
+            if len(nums) > i + j:
+                if nums[i+j] > out_arr[i]:
+                    out_arr[i] = nums[i+j]
+    return out_arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
