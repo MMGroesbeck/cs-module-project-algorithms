46ae4b816a671ada30b4085c4d1e3c279a8738db
M Groesbeck <m.groesbeck@gmail.com>
2020-07-22 12:15:39 -0700

    Initial commit; first pass of single_number working.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,11 +2,18 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+# First run: nested for loops
+# Inner loop needs to be over whole range to catch matches before current number.
 def single_number(arr):
     # Your code here
-
-    pass
-
+    for i in range(len(arr)-1):
+        match = False
+        for j in range(len(arr)):
+            if i != j and arr[i] == arr[j]:
+                match = True
+        if match == False:
+            return arr[i]
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
